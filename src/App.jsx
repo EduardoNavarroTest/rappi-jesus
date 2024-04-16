97fb42e760a51f6c4032ca2055c0cd50ba07a10e
Eduardo Enrique Navarro Echeverria <eenavarro@sprc.com.co>
2024-04-16 09:16:20 -0500

    Mejoras llamando a los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import Header from './assets/components/Header'
+import Main from './assets/components/Main'
+import Footer from './assets/components/Footer'
 
 function App() {
 
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,3 +1,6 @@
+import NavBar from "./NavBar";
+
+
 const Header = () => {
     <NavBar />
 }
