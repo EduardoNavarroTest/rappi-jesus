ec96772fafaf5a4e303767c8d6226d3b17787c02
Eduardo Enrique Navarro Echeverria <eenavarro@sprc.com.co>
2024-04-16 09:24:11 -0500

    Mejoras en imports de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Header from './assets/components/Header'
-import Main from './assets/components/Main'
-import Footer from './assets/components/Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Main from './components/Main'
 
 function App() {
 
