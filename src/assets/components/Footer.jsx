24b85279b1dd774ff6f39d1935db3691f586775a
Eduardo Enrique Navarro Echeverria <eenavarro@sprc.com.co>
2024-04-16 09:11:58 -0500

    Add form loguin

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+return (
+    <div>
+    <p>Footer (Futura implementación)</p>
+    <div/>
+)
+}
+
+export default Footer;
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,5 @@
+const Header = () => {
+    <NavBar />
+}
+
+export default Header;
